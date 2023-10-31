8b41bc34c9e98dc19f0d66add30b3b960b749bb4
Eldon McGuinness <Eldon.McGuinness@gmail.com>
2023-10-31 19:11:09 -0400

    Updated Icons / Tags

--- src/usr/local/emhttp/plugins/DriveStandbyMonitor/monitor.sh
+++ src/usr/local/emhttp/plugins/DriveStandbyMonitor/monitor.sh
@@ -3,6 +3,7 @@
 SCRIPT_NAME="DriveStandbyMonitor"
 LOG_LOCATION="/tmp/${SCRIPT_NAME}"
 PID="/var/run/${SCRIPT_NAME}.pid"
+
 # Remove the previous log
 echo "" > "${LOG_LOCATION}/monitor.log"
 
@@ -10,15 +11,25 @@ echo "" > "${LOG_LOCATION}/monitor.log"
 echo $$ > "$PID"
 
 for l in /dev/sd?; do 
-        i=$(echo $l | awk '{print substr($1,8)}'); 
-        STATUS=0
         DATE=$(date +%s)
+
+        # Get standby status info
+        i=$(echo $l | awk '{print substr($1,8)}'); 
+        STANDBY_STATUS=0
         smartctl -i -n never /dev/sd$i | grep STANDBY > /dev/null
         if [ $? -eq 1 ]; then 
-          STATUS=1
+          STANDBY_STATUS=1
         fi
-        sqlite3 "/boot/config/plugins/DriveStandbyMonitor/monitor.db" "INSERT INTO 'standby' ('drive', 'state', 'date') VALUES ('sd$i', $STATUS, $DATE);"
+
+        TEMPERATURE=$(smartctl -A /dev/sd$i | grep Temperature_Celsius | awk {'print $10'})
+        if [ "$TEMPERATURE" == "" ]; then
+                TEMPERATURE=0
+        fi
+
+        sqlite3 "/boot/config/plugins/DriveStandbyMonitor/monitor.db" "INSERT INTO 'standby' ('drive', 'state', 'temperature', 'date') VALUES ('sd$i', $STANDBY_STATUS, $TEMPERATURE, $DATE);"
 done;
 
 rm "$PID"
 exit 0;
+
+
