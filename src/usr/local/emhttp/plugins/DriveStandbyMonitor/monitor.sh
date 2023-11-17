78def146e6a8d94ffcfe150c6c6178b496711ccb
Eldon McGuinness <Eldon.McGuinness@gmail.com>
2023-11-17 14:44:48 -0500

    Added Better Labels to Drives

--- src/usr/local/emhttp/plugins/DriveStandbyMonitor/monitor.sh
+++ src/usr/local/emhttp/plugins/DriveStandbyMonitor/monitor.sh
@@ -16,15 +16,26 @@ for l in /dev/sd?; do
         # Get standby status info
         i=$(echo $l | awk '{print substr($1,8)}'); 
         STANDBY_STATUS=0
-        smartctl -i -n never /dev/sd$i | grep STANDBY > /dev/null
-        if [ $? -eq 1 ]; then 
-          STANDBY_STATUS=1
+
+        SMART=$(smartctl -i -n never /dev/sd$i)
+
+        # Check for Unknown USB Bridge, this will be things that do not support smart over USB
+        echo "$SMART" | grep "Unknown USB bridge" > /dev/null
+        if [ $? -eq 0 ]; then  # IF it matches
+          continue;
         fi
 
-        sqlite3 "/boot/config/plugins/DriveStandbyMonitor/monitor.db" "INSERT INTO 'standby' ('drive', 'state', 'date') VALUES ('sd$i', $STANDBY_STATUS, $DATE);"
+        MODEL=$(echo "$SMART" | grep "Device Model" | awk '{$1=$2=""; print $0}' | sed 's/^\s*//g' | sed 's/\s/_/g')
+        SERIAL=$(echo "$SMART" | grep "Serial Number" | awk '{$1=$2=""; print $0}' | sed 's/^\s*//g' | sed 's/\s/_/g')
+
+        DRIVE=${MODEL}_${SERIAL}
+        echo "$SMART" | grep STANDBY > /dev/null
+        if [ $? -eq 1 ]; then  #IF it does not match
+          STANDBY_STATUS=1
+        fi
+ 
+        sqlite3 "/boot/config/plugins/DriveStandbyMonitor/monitor.db" "INSERT INTO 'standby' ('drive', 'state', 'date') VALUES ('$DRIVE', $STANDBY_STATUS, $DATE);"
 done;
 
 rm "$PID"
 exit 0;
-
-
